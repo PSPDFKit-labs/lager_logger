61267fd4c97ceb955333b05051b57a36e3ec1a46
James Fish <james@fishcakez.com>
2015-03-02 23:24:11 +0000

    Configure lager

--- config/config.exs
+++ config/config.exs
@@ -1,24 +1,13 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for third-
-# party users, it should be done in your mix.exs file.
+# Stop lager redirecting :error_logger messages
+config :lager, :error_logger_redirect, false
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
+# Stop lager removing Logger's :error_logger handler
+config :lager, :error_logger_whitelist, [Logger.ErrorHandler]
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+# Stop lager writing a crash log
+config :lager, :crash_log, false
+
+# Use LagerLogger as lager's only handler.
+config :lager, :handlers, [{LagerLogger, []}]

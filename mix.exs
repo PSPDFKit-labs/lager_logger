432ebf910d33766338b531a0329a2e7a86f98d84
Alexei Sholik <alcosholik@gmail.com>
2017-02-07 21:48:14 +0100

    Bump to 1.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LagerLogger.Mixfile do
 
   def project do
     [app: :lager_logger,
-     version: "1.0.3",
+     version: "1.0.4",
      elixir: ">= 1.1.0 and < 1.5.0",
      package: package(),
      description: description(),
